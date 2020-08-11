285549071f4ee41f904e3ce5bdd9c3a31e9850b2
Alex-naumov <56648843+Alex-naumov@users.noreply.github.com>
2020-08-11 10:59:57 +0300

    исправил ошибки в 8 задании

--- task-08.js
+++ task-08.js
@@ -1,10 +1,8 @@
 import users from "./users.js";
 
 const getUsersWithFriend = (users, friendName) => {
-  const userName = users.filter((user) => {
-    user.friends.filter((friend) => friend === friendName);
-  });
-  return userName;
+  const userName = users.filter((user) => user.friends.includes(friendName));
+  return userName.name;
 };
 console.log(getUsersWithFriend(users, "Briana Decker")); // [ 'Sharlene Bush', 'Sheree Anthony' ]
 console.log(getUsersWithFriend(users, "Goldie Gentry")); // [ 'Elma Head', 'Sheree Anthony' ]
