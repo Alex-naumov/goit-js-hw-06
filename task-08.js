16dafc5ed2dca76558287247af62c9aab372f71c
Alex-naumov <56648843+Alex-naumov@users.noreply.github.com>
2020-08-11 11:30:01 +0300

    8

--- task-08.js
+++ task-08.js
@@ -1,8 +1,9 @@
 import users from "./users.js";
 
 const getUsersWithFriend = (users, friendName) => {
-  const userName = users.filter((user) => user.friends.includes(friendName));
-  return userName.name;
+  const user = users.filter((user) => user.friends.includes(friendName));
+  console.log(user.name);
+  return user.name;
 };
 console.log(getUsersWithFriend(users, "Briana Decker")); // [ 'Sharlene Bush', 'Sheree Anthony' ]
 console.log(getUsersWithFriend(users, "Goldie Gentry")); // [ 'Elma Head', 'Sheree Anthony' ]
